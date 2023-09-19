b6acfed40ecd0c745b88c55c0b287c0ef81afea4
OlgaTatcher <dai.akemi230318@gmail.com>
2023-09-19 15:37:20 +0300

    added methods countOccurance,toList,findUnique,calcOccurance,findOccurance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import phonebooks.ContactRecord;
 import phonebooks.Phonebook;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -35,14 +37,50 @@ public class Main {
             System.out.println("I can't find the contact");
         }
 
+    }
+    public static int countOccurance(List<String>words,String target) {
+        int count=0;
+        for (String word:words) {
+            if (word.equals(target))count++;
+        }
+        return count;
+    }
+    public static List<String>toList(String[]words){
+        return Arrays.asList(words);
+    }
 
+    public static List<Integer>findUnique(List<Integer>numbers){
+        List<Integer>unique=new ArrayList<>();
+        for (Integer number : numbers) {
+            if (!unique.contains(number))unique.add(number);
+        }
+        return unique;
+    }
+    public static void calcOccurance(List<String>words){
+        List<String>collector=new ArrayList<>();
+        for (String word : words) {
+            if (collector.contains(word))continue;
+            int counter=0;
+            for (String w : words) {
+                if(w.equals(words))counter++;
+            }
+            System.out.println(word+": "+counter);
+            collector.add(word);
+        }
+    }
+    public static List<String> findOccurance(List<String>words){
+        List<String>result=new ArrayList<>();
+        List<String>collect=new ArrayList<>();
 
-
-
-
-
-
-
-
+        for (String word : words) {
+            if (collect.contains(word))continue;
+            int counter=0;
+            for (String w : words) {
+                if(w.equals(words))counter++;
+            }
+            result.add("name"+"\""+word+"\""+"occurance: "+counter);
+            collect.add(word);
+        }
+        return result;
     }
 }
