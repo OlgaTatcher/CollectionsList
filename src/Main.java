106af927ca9f820a1b84f5a28e019b295eb60fa3
OlgaTatcher <dai.akemi230318@gmail.com>
2023-09-19 15:46:41 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
+import phonebooks.ContactRecord;
+import phonebooks.Phonebook;
+
 public class Main {
     public static void main(String[] args) {
-
-
-
-
-
-
-
+        Phonebook ContactList= new Phonebook();
 
 
 
--- src/phonebooks/ContactRecord.java
+++ src/phonebooks/ContactRecord.java
@@ -1,4 +1,33 @@
 package phonebooks;
 
 public class ContactRecord {
+    private String name;
+    private String phone;
+
+
+    public ContactRecord(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+    @Override
+    public String toString() {
+        return " " +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'';
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
