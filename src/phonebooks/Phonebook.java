50fd38df97b65c3b43a62efc923898369699440f
OlgaTatcher <dai.akemi230318@gmail.com>
2023-09-19 15:32:46 +0300

    added variables to Phonebook,ContactRecord added methods add,find, findInfo

--- src/phonebooks/Phonebook.java
+++ src/phonebooks/Phonebook.java
@@ -1,4 +1,41 @@
 package phonebooks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Phonebook {
+    private List<ContactRecord> contactInfo;
+
+    public Phonebook() {
+        contactInfo =new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Contact: " + contactInfo;
+    }
+
+    public void add(ContactRecord phonebookRecord){
+        contactInfo.add(phonebookRecord);
+    }
+    public ContactRecord find(String name){
+        for (ContactRecord phonebookRecord: contactInfo) {
+            if (phonebookRecord.getName().equals(name)){
+                return phonebookRecord;
+            }
+
+        }
+        return null;
+    }
+
+
+    public List<ContactRecord>  findInfo(String name) {
+        List<ContactRecord> contact = new ArrayList<>();
+        for (ContactRecord phonebookRecord : contactInfo){
+            if (phonebookRecord.getName().equalsIgnoreCase(name))contact.add(phonebookRecord);
+        }
+        if (contact.isEmpty())return null;
+
+        return contact;
+    }
 }
